573de30d6b2cb3baa22a6474a5427e72139151b0
Paul Campbell <pacampbell91@gmail.com>
2023-05-15 14:05:35 +0200

    Update server_cmds.sh

--- dev/server_cmds.sh
+++ dev/server_cmds.sh
@@ -3,11 +3,12 @@ rsync -zavh /Users/paul/epicentre/gtfcc-ocv-app/data sp:/srv/shiny-server/ocv
 rsync -zavh /Users/paul/epicentre/gtfcc-ocv-app/data episerv:/home/epicentre/gtfcc-ocv-app/
 # clear cache
 ssh shinyproxy "rm -rf /srv/shiny-server/ocv/.cache/*"
+ssh episerv "rm -rf /home/epicentre/gtfcc-ocv-app/.cache/*"
 
 # https://reports.msf.net/testing/
 docker run \
 --rm \
 -p 5858:3838 \
--v /home/epicentre/gtfcc-ocv-app:/root/app\
+-v /home/epicentre/gtfcc-ocv-app:/root/app \
  epicentremsf/shinytvgeo \
  R -e "shiny::runApp('/root/app', port = 3838, host = '0.0.0.0')"
